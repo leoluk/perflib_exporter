873b38b78d930b7a42c68198cda447b4a7296b12
Bismarck Paliz <bismarck.paliz@jet.com>
2019-04-01 13:23:53 -0400

    Multi-value counter support plus bug fixes.

--- collector/mangle.go
+++ collector/mangle.go
@@ -8,7 +8,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-func manglePerflibName(s string) (string) {
+func manglePerflibName(s string) string {
 	s = strings.ToLower(s)
 	s = strings.Replace(s, " ", "_", -1)
 	s = strings.Replace(s, ".", "", -1)
@@ -16,11 +16,12 @@ func manglePerflibName(s string) (string) {
 	s = strings.Replace(s, ")", "", -1)
 	s = strings.Replace(s, "+", "", -1)
 	s = strings.Replace(s, "-", "", -1)
+	s = strings.Replace(s, ",", "", -1)
 
 	return s
 }
 
-func manglePerflibCounterName(s string) (string) {
+func manglePerflibCounterName(s string) string {
 	s = manglePerflibName(s)
 
 	s = strings.Replace(s, "total_", "", -1)
@@ -43,8 +44,12 @@ func manglePerflibCounterName(s string) (string) {
 func MakePrometheusLabel(def *perflib.PerfCounterDef) (s string) {
 	s = manglePerflibCounterName(def.Name)
 
-	if def.IsCounter {
-		s += "_total"
+	if len(s) > 0 {
+		if IsCounter(def.CounterType) {
+			s += "_total"
+		} else if IsBaseValue(def.CounterType) && !strings.HasSuffix(s, "_base") {
+			s += "_max"
+		}
 	}
 
 	return
@@ -54,13 +59,13 @@ func pdhNameFromCounterDef(obj perflib.PerfObject, def perflib.PerfCounterDef) s
 	return fmt.Sprintf(`\%s(*)\%s`, obj.Name, def.Name)
 }
 
-func descFromCounterDef(obj perflib.PerfObject, def perflib.PerfCounterDef) (string, *prometheus.Desc) {
+func descFromCounterDef(obj perflib.PerfObject, def perflib.PerfCounterDef) *prometheus.Desc {
 	subsystem := manglePerflibName(obj.Name)
 	counterName := MakePrometheusLabel(&def)
 
 	labels := []string{"name"}
 
-	if def.Name == "" {
+	if len(obj.Instances) == 1 {
 		labels = []string{}
 	}
 
@@ -68,13 +73,14 @@ func descFromCounterDef(obj perflib.PerfObject, def perflib.PerfCounterDef) (str
 		labels = append(labels, PromotedLabelsForObject(obj.NameIndex)...)
 	}
 
-	if HasMergedLabels(obj.NameIndex) {
-		s, labelsForObject := MergedLabelsForInstance(obj.NameIndex, def.NameIndex)
-		counterName = s
-		labels = append(labels, labelsForObject)
-	}
+	// TODO - Label merging needs to be fixed for [230] Process
+	//if HasMergedLabels(obj.NameIndex) {
+	//	s, labelsForObject := MergedLabelsForInstance(obj.NameIndex, def.NameIndex)
+	//	counterName = s
+	//	labels = append(labels, labelsForObject)
+	//}
 
-	return counterName, prometheus.NewDesc(
+	return prometheus.NewDesc(
 		prometheus.BuildFQName(Namespace, subsystem, counterName),
 		fmt.Sprintf("perflib metric: %s (see /dump for docs) [%d]",
 			pdhNameFromCounterDef(obj, def), def.NameIndex),
